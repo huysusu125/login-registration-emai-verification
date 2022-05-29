361939f3a34a32ad9bdb281a5480e5a987ae735b
huytd <huy.td173171@sis.hust.edu.vn>
2022-05-29 14:01:10 +0700

    start

---
+++ src/main/java/com/example/loginregistrationemailverification/LoginRegistrationEmailVerificationApplication.java
@@ -0,0 +1,13 @@
+package com.example.loginregistrationemailverification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginRegistrationEmailVerificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginRegistrationEmailVerificationApplication.class, args);
+    }
+
+}
